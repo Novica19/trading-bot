cf89f820b2b878b0d815734530b678fcabb9ac6f
Novica19 <you@example.com>
2022-09-21 13:38:59 +0200

    API keys added

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ if __name__ == '__main__':
 
 
     # HERE INSERT YOUR API KEYS
-    binance = BinanceFuturesClient(config.api_key_testnet_binance, config.api_secret_testnet_binance, True)
-    bitmex = BitmexClient(config.api_key_testnet_bitmex, config.api_secret_testnet_bitmex, True))
+    binance = BinanceFuturesClient("61f9472a2e2b9027ea5fa69045a83b62adc2706d23ff9b89c0550bbe5d9c55e5", "a8afaf411b4410aa0942af1a289989826595f5f21abbf9975b2fea67078de6c2", True)
+    bitmex = BitmexClient("bT1DPuWY35kcmX7Zp_p8MRQl", "9mH13sOVtW_QUs2Kms247r9wLmQDjAZ_ls3J1sdIxO0pZmzD", True)
 
     root = Root(binance, bitmex)
-    root.mainloop()
+    root.mainloop() 
